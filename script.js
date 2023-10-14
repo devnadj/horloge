bf8f3a99f8a9bcd0fe541d6490225c5142a4d221
devnadj <dev.nadj@hotmail.com>
2023-10-14 17:45:38 +0200

    horloge avec affichage sous forme de leds

--- script.js
+++ script.js
@@ -1,102 +1,115 @@
-/**
-* Afficher l'heure sous forme textuelle
-* 
-* Ici rien de bien complexe, on récupère la date courante, ensuite on 
-* sépare les heures, les minutes et les secondes dans trois variables.
-* Si ces variables sont inférieures à 10, on ajoute un 0 devant le chiffre
-* histoire de rendre l'affichage plus agréable. Ensuite il suffit 
-* d'afficher le résultat dans un élément <p> sur la page
-* 
-*/
+const police = {
+    A : [[0,1,1,1,0],[1,0,0,0,1],[1,0,0,0,1],[1,0,0,0,1],[1,1,1,1,1],[1,0,0,0,1],[1,0,0,0,1]],
+    B : [[1,1,1,1,0],[1,0,0,0,1],[1,0,0,0,1],[1,1,1,1,0],[1,0,0,0,1],[1,0,0,0,1],[1,1,1,1,0]],
+    C : [[0,1,1,1,0],[1,0,0,0,1],[1,0,0,0,0],[1,0,0,0,0],[1,0,0,0,0],[1,0,0,0,1],[0,1,1,1,0]],
+    D : [[1,1,1,1,0],[1,0,0,0,1],[1,0,0,0,1],[1,0,0,0,1],[1,0,0,0,1],[1,0,0,0,1],[1,1,1,1,0]],
+    E : [[1,1,1,1,1],[1,0,0,0,0],[1,0,0,0,0],[1,1,1,1,0],[1,0,0,0,0],[1,0,0,0,0],[1,1,1,1,1]],
+    F : [[1,1,1,1,1],[1,0,0,0,0],[1,0,0,0,0],[1,1,1,1,0],[1,0,0,0,0],[1,0,0,0,0],[1,0,0,0,0]],
+    G : [[0,1,1,1,0],[1,0,0,0,1],[1,0,0,0,0],[1,0,1,1,1],[1,0,0,0,1],[1,0,0,0,1],[0,1,1,1,0]],
+    H : [[1,0,0,0,1],[1,0,0,0,1],[1,0,0,0,1],[1,1,1,1,1],[1,0,0,0,1],[1,0,0,0,1],[1,0,0,0,1]],
+    I : [[1,1,1,1,1],[0,0,1,0,0],[0,0,1,0,0],[0,0,1,0,0],[0,0,1,0,0],[0,0,1,0,0],[1,1,1,1,1]],
+    J : [[0,1,1,1,1],[0,0,0,1,0],[0,0,0,1,0],[0,0,0,1,0],[0,0,0,1,0],[1,0,0,1,0],[0,1,1,0,0]],
+    K : [[1,0,0,0,1],[1,0,0,1,0],[1,0,1,0,0],[1,1,0,0,0],[1,0,1,0,0],[1,0,0,1,0],[1,0,0,0,1]],
+    L : [[1,0,0,0,0],[1,0,0,0,0],[1,0,0,0,0],[1,0,0,0,0],[1,0,0,0,0],[1,0,0,0,0],[1,1,1,1,1]],
+    M : [[1,0,0,0,1],[1,1,0,1,1],[1,0,1,0,1],[1,0,1,0,1],[1,0,0,0,1],[1,0,0,0,1],[1,0,0,0,1]],
+    N : [[1,0,0,0,1],[1,1,0,0,1],[1,0,1,0,1],[1,0,0,1,1],[1,0,0,0,1],[1,0,0,0,1],[1,0,0,0,1]],
+    O : [[0,1,1,1,0],[1,0,0,0,1],[1,0,0,0,1],[1,0,0,0,1],[1,0,0,0,1],[1,0,0,0,1],[0,1,1,1,0]],
+    P : [[1,1,1,1,0],[1,0,0,0,1],[1,0,0,0,1],[1,1,1,1,0],[1,0,0,0,0],[1,0,0,0,0],[1,0,0,0,0]],
+    Q : [[0,1,1,1,0],[1,0,0,0,1],[1,0,0,0,1],[1,0,0,0,1],[1,0,1,0,1],[1,0,0,1,0],[0,1,1,0,1]],
+    R : [[1,1,1,1,0],[1,0,0,0,1],[1,0,0,0,1],[1,1,1,1,0],[1,0,0,0,1],[1,0,0,0,1],[1,0,0,0,1]],
+    S : [[0,1,1,1,0],[1,0,0,0,1],[1,0,0,0,0],[0,1,1,1,0],[0,0,0,0,1],[1,0,0,0,1],[0,1,1,1,0]],
+    T : [[1,1,1,1,1],[0,0,1,0,0],[0,0,1,0,0],[0,0,1,0,0],[0,0,1,0,0],[0,0,1,0,0],[0,0,1,0,0]],
+    U : [[1,0,0,0,1],[1,0,0,0,1],[1,0,0,0,1],[1,0,0,0,1],[1,0,0,0,1],[1,0,0,0,1],[0,1,1,1,0]],
+    V : [[1,0,0,0,1],[1,0,0,0,1],[1,0,0,0,1],[1,0,0,0,1],[1,0,0,0,1],[0,1,0,1,0],[0,0,1,0,0]],
+    W : [[1,0,0,0,1],[1,0,0,0,1],[1,0,0,0,1],[1,0,1,0,1],[1,0,1,0,1],[1,0,1,0,1],[0,1,0,1,0]],
+    X : [[1,0,0,0,1],[1,0,0,0,1],[0,1,0,1,0],[0,0,1,0,0],[0,1,0,1,0],[1,0,0,0,1],[1,0,0,0,1]],
+    Y : [[1,0,0,0,1],[1,0,0,0,1],[1,0,0,0,1],[0,1,0,1,0],[0,0,1,0,0],[0,0,1,0,0],[0,0,1,0,0]],
+    Z : [[1,1,1,1,1],[0,0,0,0,1],[0,0,0,1,0],[0,0,1,0,0],[0,1,0,0,0],[1,0,0,0,0],[1,1,1,1,1]],
+    '0':[[0,1,1,1,0],[1,0,0,0,1],[1,0,0,1,1],[1,0,1,0,1],[1,1,0,0,1],[1,0,0,0,1],[0,1,1,1,0]],
+    '1':[[0,0,1,0,0],[0,1,1,0,0],[0,0,1,0,0],[0,0,1,0,0],[0,0,1,0,0],[0,0,1,0,0],[0,1,1,1,0]],
+    '2':[[0,1,1,1,0],[1,0,0,0,1],[0,0,0,0,1],[0,0,0,1,0],[0,0,1,0,0],[0,1,0,0,0],[1,1,1,1,1]],
+    '3':[[0,1,1,1,0],[1,0,0,0,1],[0,0,0,0,1],[0,0,1,1,0],[0,0,0,0,1],[1,0,0,0,1],[0,1,1,1,0]],
+    // '4':[[1,0,0,1,0],[1,0,0,1,0],[1,0,0,1,0],[1,1,1,1,1],[0,0,0,1,0],[0,0,0,1,0],[0,0,0,1,0]],
+    '4':[[0,0,0,1,0],[0,0,1,1,0],[0,1,0,1,0],[1,0,0,1,0],[1,1,1,1,1],[0,0,0,1,0],[0,0,0,1,0]],
+    '5':[[1,1,1,1,1],[1,0,0,0,0],[1,0,0,0,0],[1,1,1,1,0],[0,0,0,0,1],[1,0,0,0,1],[0,1,1,1,0]],
+    '6':[[0,1,1,1,0],[1,0,0,0,1],[1,0,0,0,0],[1,1,1,1,0],[1,0,0,0,1],[1,0,0,0,1],[0,1,1,1,0]],
+    '7':[[1,1,1,1,1],[0,0,0,0,1],[0,0,0,1,0],[0,0,1,0,0],[0,1,0,0,0],[0,1,0,0,0],[0,1,0,0,0]],
+    '8':[[0,1,1,1,0],[1,0,0,0,1],[1,0,0,0,1],[0,1,1,1,0],[1,0,0,0,1],[1,0,0,0,1],[0,1,1,1,0]],
+    '9':[[0,1,1,1,0],[1,0,0,0,1],[1,0,0,0,1],[0,1,1,1,1],[0,0,0,0,1],[1,0,0,0,1],[0,1,1,1,0]],
+    ':':[[0,0,0,0,0],[0,0,1,0,0],[0,0,1,0,0],[0,0,0,0,0],[0,0,1,0,0],[0,0,1,0,0],[0,0,0,0,0]]
 
-    /**
-     * Afficher maintenant un cadrant et des aiguilles est autrement plus 
-     * complexe. La première chose consiste à afficher le cadrant. Une <div>
-     * sera suffisante : mais il faut lui donner la forme adéquate :
-     * .cadran {
-     *     border-radius: 50%
-     * }
-     * 
-     * une bordure, pour bien la distinguer
-     * 
-     * border: 1px solid #000;
-     * 
-     * Maintenant ajouter quelques indications d'heures sur le cadran pour faciliter
-     * la lecture. Commençons par quatre indications : 3h, 6h, 9h et 12h. Ce seront
-     * les plus facile à positionner.
-     * 
-     * Le cadran doit être en position relative, et les indications seront en position
-     * absolue.
-     * 
-     * .cadran { position: relative; }
-     * .number { position: absolute; }
-     * 
-     * chaque nombre possède sa propre classe, ce qui permet de la positionner 
-     * indépendemment des autres. Le positionnement se fait avec les propriétés
-     * CSS « top, bottom, left et right ». Le positionnement n'est pas tout à fait
-     * centré puisqu'il se fait par rapport au coin supérieur gauche d'un élément
-     * et non par son centre. Il faut donc pour chaque élement positionné appliquer
-     * une translation.
-     * 
-     * .n9 {
-     *     top: 50%;
-     *     left: 5%
-     *     transform: translation(0, -50%)
-     * }
-     * 
-     */
+    
 
-    /**
-     * Une fois le cadran et les nombres positionnés, il faut maintenant s'occuper de
-     * l'affichage des aiguilles. Le plus simple à mon avis est d'utiliser un svg, avec
-     * deux lignes qui symboliseront nos aiguilles. Les coordonnées des points des lignes
-     * seront calculées en fonction de l'heure (ou des minutes). Il faut savoir qu'un SVG
-     * est un élément HTML comme les autres. Il est ainsi possible pour chaque sous-élément
-     * constituant un SVG de lui attribuer des classes, et par la même de modifier ses 
-     * propriétés via CSS.
-     * 
-     * Dans les code suivant :
-     * 
-     * <svg class="pointers">
-     *     <line class="hours" x1="100" x2="150" y1="100" y2="150" stroke="black" />
-           <line class="minutes" x1="100" y1="100" x2="50" y2="50" stroke="black" />
-     * </svg>
-     * 
-     * deux lignes noires partant du centre son ajoutéées.
-     * 
-     * à chacune des lignes est attribué une classe. Ce qui permet par CSS de modifier
-     * .hours {
-     *     stroke: red;
-     * }
-     * 
-     * .minutes {
-     *    stroke: blue;
-     * }
-     * 
-     * De plus il est possible de modifier les coordonnées de départ et d'arrivée d'une ligne
-     * grâce aux attribut (x1, y1) et (x2, y2). Pour cela il suffira simplement d'utiliser
-     * .setAttribute('attribut-à-modifier', 'nouvelle-valeur)
-     * 
-     * Maintenant il suffit de déterminer l'angle des minutes / heures pour tracer nos lignes
-     * Pour rappel, sur un cerle :
-     *      3h / 15h correspondent à 0° ou 360°
-     *      12h correspond à 90°
-     *      9h correpond à 180°
-     *      6h / 18h corres
-     * 
-     * 
-     **/
+}
+
+class Matrix {
+    matrix;
+    height;
+    width;
+    constructor(x, y) {
+        this.height = y;
+        this.width = x;
+        this.matrix = Array(x).fill(Array(y).fill(0));
+    }
+
+    show() {
+        const matrixElement = document.querySelector('.matrix')
+        for(let yCounter = 0; yCounter < this.height; yCounter++) {
+            const line = document.createElement('div');
+            line.classList.add('line');
+            line.classList.add(`l${yCounter}`)
+            matrixElement.append(line);
+            for(let xCounter = 0; xCounter < this.width; xCounter ++) {
+                const del = document.createElement('div');
+                del.classList.add('del');
+                del.classList.add(`c${xCounter}`)
+                line.append(del);
+            }  
+        }
+    }
+
+    clear(color) {
+        const dels = document.querySelectorAll('.del');
+        console.log(dels);
+        for(const del of dels) {
+            del.style.backgroundColor = color;
+            //console.log(del);
+
+        }
+    }
+
+    set(x, y, color) {
+        if(y >= this.height || x >= this.width) {
+            throw new Error('index out of range');
+            console.log('.... . ..')
+        }
+        else {
+            document.querySelector(`.l${y} .c${x}`).style.backgroundColor = color;
+        }
+    }
+
+    addLetter(letter, position) {
+        for(let y = 0; y < 7; y ++) {
+            for(let x = 0; x < 5; x++) {
+                if(police[letter][y][x] !== 0)
+                    this.set(x + position.x, y + position.y, '#ccc');
+            }
+        }
+    }
+}
+
+const heure = new Matrix(45, 9);
+heure.show();
 
 function showTime() {
 
     const date = new Date();
-    let hours = date.getHours();
-    let minutes = date.getMinutes();
-    
-    let seconds = date.getSeconds();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
     document.querySelector('p').textContent = `${hours < 10? '0' + hours : hours}:${minutes < 10 ? '0' + minutes : minutes}:${seconds < 10 ? '0' + seconds:seconds}`;
 
-    const angleHours = hours > 12 ? (hours -12)  * (360/12) -90 : hours * (360/12) - 90
+    const angleHours = hours > 12 ? 0.5 * minutes + (hours -12)  * (360/12) -90 : 0.5 * minutes + hours * (360/12) - 90
     const angleMinutes = minutes * (360/60) - 90;
     const angleSeconds = seconds * (360/60) - 90;
     
@@ -105,12 +118,13 @@ function showTime() {
     const minutesHand = document.querySelector('.minutes');
 
     /* version en modifiant le CSS */
+    /*
     secondsHand.style.transform = `rotate(${angleSeconds}deg)`;
     minutesHand.style.transform = `rotate(${angleMinutes}deg)`;
     hoursHand.style.transform = `rotate(${angleHours}deg)`;
+    */
 
     /* version en jouant sur les attributs x2 et y2 des lignes */
-    /*
     const x2Hours = 100 + 70 * Math.cos (Math.PI * angleHours / 180);
     const y2Hours = 100 + 70 * Math.sin (Math.PI * angleHours / 180);
     hoursHand.setAttribute('x2', x2Hours)
@@ -121,12 +135,44 @@ function showTime() {
     minutesHand.setAttribute('x2', x2Minutess)
     minutesHand.setAttribute('y2', y2Minutess)
 
-    const x2Seconds = 100 + 80 * Math.cos (Math.PI * angleSeconds / 180);
-    const y2Seconds = 100 + 80 * Math.sin (Math.PI * angleSeconds / 180);
-    console.log('x2:' , x2Seconds, 'y2: ', y2Seconds);
+    const x2Seconds = 100 + 90 * Math.cos (Math.PI * angleSeconds / 180);
+    const y2Seconds = 100 + 90 * Math.sin (Math.PI * angleSeconds / 180);
+    // console.log('x2:' , x2Seconds, 'y2: ', y2Seconds);
     secondsHand.setAttribute('x2', x2Seconds);
     secondsHand.setAttribute('y2', y2Seconds);
-    */
+
+    /* séparer les caractères de l'heure */
+    const unitsDigitHours = hours %10;
+    const tensDigitHours = (hours - unitsDigitHours) / 10;
+    console.log('Hours : ', tensDigitHours,unitsDigitHours);
+    
+    const unitsDigitMinutes = minutes %10;
+    const tensDigitMinutes = (minutes - unitsDigitMinutes) / 10;
+    console.log('Minutes : ', tensDigitMinutes, unitsDigitMinutes);
+
+    const unitsDigitSeconds = seconds %10;
+    const tensDigitSeconds = (seconds - unitsDigitSeconds) / 10;
+    console.log('Secondes : ', tensDigitSeconds, unitsDigitSeconds);
+
+
+    heure.clear('black');
+    heure.addLetter(tensDigitHours, {x:1, y:1});
+    heure.addLetter(unitsDigitHours, {x:7, y:1});
+    heure.set(14, 2, 'red')
+    heure.set(14, 3, 'red')
+    heure.set(14, 5, 'red')
+    heure.set(14, 6, 'red')
+    heure.addLetter(tensDigitMinutes, {x:17, y:1});
+    heure.addLetter(unitsDigitMinutes, {x:23, y:1});
+    heure.set(30, 2, 'red')
+    heure.set(30, 3, 'red')
+    heure.set(30, 5, 'red')
+    heure.set(30, 6, 'red')
+    heure.addLetter(tensDigitSeconds, {x:33, y:1});
+    heure.addLetter(unitsDigitSeconds, {x:39, y:1});
 }
 
 setInterval(showTime, 1000);
+
+
+
