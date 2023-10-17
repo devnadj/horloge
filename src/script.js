3a9c3a50c3231f730aa4d53f311bf2a4687aa6b3
devnadj <dev.nadj@hotmail.com>
2023-10-18 01:18:08 +0200

    ajout d'une balle supplémetaire

--- src/script.js
+++ src/script.js
@@ -12,6 +12,6 @@ digitalClock.show();
 setInterval(function(){digitalClock.updateTime()}, 1000);
 
 const rebound = new Rebound(document.querySelector('.rebound'), 48, 16, '#222', [{ x:0, y:0, color:'#F00'},
-{x:47, y:0, color:'#0F0'},{x:15, y:15, color:'#00F'}, {x:3, y:15, color:'#FFF'}]);;
+{x:47, y:0, color:'#0F0'},{x:15, y:15, color:'#00F'}, {x:3, y:15, color:'#FFF'}, {x:12, y:1, color:'yellow'}]);;
 rebound.show();
 setInterval(function(){rebound.update()}, 100);
